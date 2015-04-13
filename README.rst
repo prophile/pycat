e824de8058d884b088f9ad7e77c2854db94bbf1d
Alistair Lynn <arplynn@gmail.com>
2015-04-13 21:10:19 +0100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,6 @@
+pycat
+=====
+
+netcat, reimplemented in Python 3. Part of a programming exercise.
+
+A subset of features only.
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+
+with open('README.rst') as f:
+    long_description = f.read()
+
+setup(name='pycat',
+      version='0.1.0',
+      packages=find_packages(),
+      description='netcat, in Python',
+      long_description=long_description,
+      author='Alistair Lynn',
+      author_email='alistair@alynn.co.uk',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet',
+        'Topic :: System :: Networking',
+        'Topic :: Utilities'
+      ],
+      setup_requires=[
+        'nose >=1.3, <2'
+      ],
+      entry_points={
+        'console_scripts': [
+          'pycat = pycat.cli:main'
+        ]
+      })

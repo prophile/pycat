571829da26c322d26429fe3341fbc75eff7ac702
Alistair Lynn <arplynn@gmail.com>
2015-04-13 22:26:11 +0100

    Fix the test

--- pycat/talk.py
+++ pycat/talk.py
@@ -4,12 +4,22 @@ import sys
 import select
 
 
-def talk(socket, source=sys.stdin.buffer, sink=sys.stdout.buffer):
+def talk(socket, source=None, sink=None):
     """Run communication, in a loop. Input from `source` is sent on `socket`,
     and data received on `socket` is forwarded to `sink`.
 
+    If `source` or `sink` are not provided, sys.stdin and sys.stdout are used.
+
     All file descriptors must be non-blocking.
     """
+
+    # Query these here, rather than in the arguments, to appease nose.
+    # Nose, through its mechanism for capturing stdout, doesn't have a .buffer
+    # on sys.stdout, so this causes an error when the module is imported.
+    if source is None:
+        source = sys.stdin.buffer
+    if sink is None:
+        sink = sys.stdout.buffer
 
     OUTPUT_BUFFER_SIZE = 1024
 
